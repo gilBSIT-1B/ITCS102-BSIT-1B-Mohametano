37e071c57d1be709d3bf0208ebba98102c4a1c81
gilBSIT-1B <gilcrismohametano01@gmail.com>
2025-08-23 16:31:09 +0800

    Update activity.5.py

--- activity.5.py
+++ activity.5.py
@@ -1,3 +1,6 @@
-name = eval(input("Type any character here . .  ."))
+#name = eval(input("Type any character here-->"))
 
+#print("The data type of name is", type(name))
+
+name = eval(input("Type any number or word here..."))
 print("The data type of name is ", type(name))
