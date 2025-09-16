0ea0550274680b4f0083f66678b51ce2e628fcd4
gilBSIT-1B <gilcrismohametano01@gmail.com>
2025-09-16 11:29:58 +0800

    Update codechallenge5.py

--- codechallenge5.py
+++ codechallenge5.py
@@ -2,8 +2,9 @@ print("Odd number Summation")
 
 tot = 0
 
-for x in range(0, 7, 1):
+for x in range(0, 10, 1):
     n = eval(input("Enter any number---> "))
     if n % 2:
         tot += n
+
 print("The sum of all the given odd number is",tot)
