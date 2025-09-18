c367c12b2deb2379057dda6641b56a2a383b1073
gilBSIT-1B <gilcrismohametano01@gmail.com>
2025-09-18 11:40:18 +0800

    Update code_challenge9.py

--- code_challenge9.py
+++ code_challenge9.py
@@ -3,4 +3,4 @@ x = str(input("What do you want your parrot to say?: "))
 print("How many do you want your parrot to say", x,"?")
 xy = int(input(""))
 for xyz in range(0, xy, 1):
-    print(x)
+    print("Squackk!!!",x)
