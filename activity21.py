d85c6c3cdda6672d6466c456f85bd4dbce911fe9
gilBSIT-1B <gilcrismohametano01@gmail.com>
2025-10-27 15:52:53 +0800

    Add files via upload

--- activity21.py
+++ activity21.py
@@ -2,12 +2,29 @@ isdirty = True
 sum = 0
 while isdirty == True:
     confirm = input("Do you want to continue washing ( yes / no ): ").lower()
-    sum += 1
     if confirm == 'yes':
         print("Washing Washing continues...")
+        sum += 1
         continue
-    else:
+    elif confirm== 'no':
         print("Washing Washing stops...")
         break
+    else:
+        print("yes or no?")
 
 print("The number of cycle is", sum)
+mo = 0
+import random
+guessed = True
+while guessed == True:
+    guess = random.randint(1,10)
+    x = int(input(f"Guess the Random number (1,10): "))
+    mo += 1
+    if x == guess:
+        print("Congratulations you've guessed it right!!!")
+        break
+        
+    else:
+        print("ENGKKKK!!, Guess again")
+        continue
+print(f"You've guessed right in your {mo}'th try.")
