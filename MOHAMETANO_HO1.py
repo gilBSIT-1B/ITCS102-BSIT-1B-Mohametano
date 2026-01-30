a2cff87dadb3b76fbb1e30bd9b5d27ca538b53d9
gilBSIT-1B <gilcrismohametano01@gmail.com>
2026-01-30 10:49:45 +0800

    Add files via upload

--- MOHAMETANO_HO1.py
+++ MOHAMETANO_HO1.py
@@ -1,10 +1,14 @@
 inp = input("type ka ng word bossing: ")
 inpn = len(inp)
 n = 0
-print(f"Ang nilagay mo boss na word ay may {inpn} na letters")
+num1 = []
+print(f"Ang nilagay mo boss na word ay may {inpn} na letters\n")
 for i in range (1,inpn+1):
     num = int(input(f"type ka ng number bossing {i}: "))
     n += num
-print(f"Ang total ng tinype mong numbers ay: {n}")
+    num1.append(num)
+ 
+print(f"Ang mga tinype mo boss na number ay \n",num1)
+print(f"Ang total ng tinype mong numbers ay: {n}\n")
 ave = n / inpn
 print(f"Ang average ng tinype mong numbers ay: {ave}")
